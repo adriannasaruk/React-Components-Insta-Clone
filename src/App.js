dc4b797c784b5975a3f06c7fbc818e4c41af79bf
Adrian Nasaruk <adriannasaruk@gmail.com>
2020-02-18 13:56:47 -1000

    finished MVP

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import PostsPage from "./components/PostsContainer/PostsPage.js";
+import SearchBar from "./components/SearchBar/SearchBarContainer.js";
 // import the PostsPage and SearchBar and add them to the App
+
 
 
 const App = () => {
 
   return (
     <div className="App">
+     <SearchBar />
+     <PostsPage />
+     
       {/* Add imported components here to render them */}
+      
     </div>
   );
 };
